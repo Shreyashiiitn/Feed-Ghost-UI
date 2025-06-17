b245a03e608ace94dc361038d94c1b003422f1ed
Shreyash Rajurkar <shreyashraj712004@gmail.com>
2025-06-17 14:16:46 +0530

    fix

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -7,37 +7,37 @@ import { User } from 'next-auth';
 import { authOptions } from '../../auth/[...nextauth]/options';
 import { NextRequest } from 'next/server';
 
+type Props = {
+  params: {
+    messageid: string;
+  };
+};
+
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { messageid: string } }
+  context: Props
 ) {
-  const messageId = params.messageid;
-
+  const messageId = context.params.messageid;
   await dbConnect();
-
   const session = await getServerSession(authOptions);
   const _user: User = session?.user as User & { _id: string }; 
-
   if (!session || !_user) {
     return Response.json(
       { success: false, message: 'Not authenticated' },
       { status: 401 }
     );
   }
-
   try {
     const updateResult = await UserModel.updateOne(
       { _id: _user._id },
       { $pull: { messages: { _id: messageId } } }
     );
-
     if (updateResult.modifiedCount === 0) {
       return Response.json(
         { message: 'Message not found or already deleted', success: false },
         { status: 404 }
       );
     }
-
     return Response.json(
       { message: 'Message deleted', success: true },
       { status: 200 }
